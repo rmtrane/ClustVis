f841e47aeaf16a79db7b378aac06d93bb2883e7d
rmtrane <rmtrane@gmail.com>
2020-07-06 12:33:49 -0500

    Add here::here back in source

--- global.R
+++ global.R
@@ -23,7 +23,7 @@ if(!dir.exists(sessPathLarge))
   dir.create(sessPathLarge)
 
 
-source("./Rpackage/R/clustvis.R")
+source(here::here("Rpackage/R/clustvis.R"))
 
 # .libPaths(libPath)
 suppressPackageStartupMessages({
